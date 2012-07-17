3deadecc04773d3f44845732e6bcfdb2593c362e
Peter Halacsy <peter.halacsy@prezi.com>
2012-07-17 21:16:07 +0200

    Totally new modell for a type safe dependency graph

--- logic.hs
+++ logic.hs
@@ -1,166 +1,97 @@
-module Logic(
-	Rule(Rule)
-	,AnchoredRule(AnchoredRule)
-	, anchor_rule
-	, File(name)
-	, (#)
-	, (##)
-	, Params
-	, Cmd
-	, make
-	, substitute
-	, create_file
-	, params_from_list
-	, rules_to_tree
-) where
-import Data.List.Utils(join)
-import Data.Maybe
-import Hdfs
+module Logic where
 import Data.Time.LocalTime
 
-import qualified Data.Map.Strict as Map
-
-data File = File { name :: String  
-				     , gen :: Params -> String
-                     , pnames :: [String]  
-                     , anchored_parameters :: Params 
-                     }
-
---create_file::String->[(String, String)] -> String->[String] ->[(String, String)]  
-create_file name gen pnames anchored_parameters = File {name = name, gen = gen, pnames = pnames, anchored_parameters=anchored_parameters}
-
-params_from_list::[(String, String)]->Params
-params_from_list = Map.fromList
-
-anchored_name::File->String
-anchored_name f = (gen f)  (anchored_parameters f)
-
-not_anchored_params::File->[String]
-not_anchored_params f =
-	let anchored = Map.keys (anchored_parameters f) in
-	filter (`notElem` anchored) (pnames f)
-
--- file_from_string s = File {name = s, pnames=[], anchored_parameters=[]}
-is_fully_anchored::File -> Bool
-is_fully_anchored f =
-	length (not_anchored_params f) == 0
-
-instance Show File where
-    show f =  
-    	let anchored = anchored_parameters f in
-    	let str_anchored = map (\(a,b) -> a ++ "=" ++ b)  (Map.assocs anchored) in
-    	let not_anchored = not_anchored_params f in
-    	if is_fully_anchored f then
-			((gen f) (anchored_parameters f)) 
-		else
-			(name f) ++  ":[" ++ (join "," ( not_anchored)) ++ "/" ++  (join ","  str_anchored) ++ "]" 
-
-substitute::File->Params->File
-substitute f params =
-	Map.foldlWithKey aux f params
-	--aux::File->String->String->String
-	where aux f k v =
-		if notElem k (pnames f) then error (k ++ " is not parameter of " ++ (name f)) else
-		create_file (name f) (gen f) (pnames f) (Map.insert k v (anchored_parameters f) )
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 
-type Params = Map.Map String String
+{-
+cat napi_log-$y-$m-$d | grep "user" > user-$y-$m-$d
+cat user-$y-$m-$d=(day_of_month $y $m) | sort | uniq > monthy-user-$y-$m
+
+-}
+
+-- van-e output? meg van-e minden input
+-- le_kell_e_futtatni::String->[String]->IO Bool
+
 type Cmd = String
+type File = String
+isMoreRecent (dep:deps) reference = True
 
-type CmdGen =  (Params -> ([File], Cmd))
-data Rule = Rule File CmdGen
-data AnchoredRule = AnchoredRule File [File] Cmd deriving Show
+generateCommand deps myOutput cmd
+  | deps `isMoreRecent` myOutput = [cmd]
+  | otherwise                    = []
 
-(#)::Params->String->String
-(#) params k = case Map.lookup k params of
-		Just v -> v
-		_ -> error ("can't find " ++ k ++ " in " ++ (show params))
 
-(##)::Params->(String, String)->Params
-(##) params (k,v) = Map.insert k v params
 
-anchor_rule::File->Rule->Maybe AnchoredRule
-anchor_rule file (Rule output cmdGen) =
-	-- paste a-1 > d 
-	-- paste a-$date > c-[date=1]
-	if not (is_fully_anchored file) then error ("can't anchor rule to generate abstract file " ++ (show file)) else
-	let params = (anchored_parameters file) in
-	
+data DepGraph = 
+	InputFile File |
+	GeneratedFile [DepGraph] File Cmd deriving (Show)
 
-	if name output /= name file then Nothing else
-	-- TODO: covers?
-	-- sort a-$date > c-$date can't generate c (without any argument)
-	--if pnames output /= pnames file then Nothing else
+output::DepGraph->File
+output (InputFile f) = f
+output (GeneratedFile _ f _)  = f
 
-	let (inputs, cmd) = cmdGen params in
-	Just (AnchoredRule (substitute output params) inputs cmd)
+have_to_generate::File->[DepGraph]->IO Bool
+have_to_generate "user-2012-5-1" _ = return True
+have_to_generate _ _ = return False
 
-data Tree =  Node AnchoredRule [Tree] | Term deriving (Show)
 
-rules_to_tree::[Rule]->File->Tree
-rules_to_tree rules f =
-    case mapMaybe (anchor_rule f) rules of
-        [] -> Term
-        rule@(AnchoredRule output inputs cmd):_ -> Node rule (map (rules_to_tree rules) inputs )
-
-should_be_rerun_by_stat::Maybe LocalTime->[Maybe LocalTime]->Bool
--- if the output doesn't exist -> must run
-should_be_rerun_by_stat Nothing _ = True
-should_be_rerun_by_stat (Just time) inputs = 
-	foldl needed False inputs where
-		needed True _ = True
-		needed _ Nothing = True
-		needed _ (Just t) 
-			| t > time = True
-			| otherwise = False
-
-stat_file = last_modified2
-
-should_be_rerun::AnchoredRule->IO Bool    
-should_be_rerun (AnchoredRule output inputs _) =
-	do
-		o <- stat_file $ anchored_name output
-		dates <-  sequence (map stat_file  (map anchored_name inputs))
-		return (should_be_rerun_by_stat o dates)
-
--- selects subtree which should be executed
-select::Tree->IO Tree
-select Term = return Term
-select (Node r childs) = -- this must be executed if any of it's child
+-- selects subgraph which should be executed
+-- return either InputFile if no execution needed
+-- or GeneratedFile otherwise
+select::DepGraph->IO DepGraph
+select (InputFile f) = return $ InputFile f
+select (GeneratedFile deps output cmd) = 
 		do
-                x <- should_be_rerun r
-                c <- sequence $ map select childs
-                let ct = filter is_not_term c in
-                 if (null ct) && (not x) then
-                    return Term
+			-- this must be executed iif output is too old
+            too_old <- have_to_generate output deps
+            --  any of it's child must be genereted
+            childs <-  sequence $ map select deps
+            let child_to_gen = filter generated childs
+            if (null child_to_gen) && (not too_old) then
+                    return (InputFile output)
                  else 
-                    return (Node r ct)
+                    return (GeneratedFile childs output cmd)
                  where
                  -- maybe there is a shorter version of this
-                 is_not_term Term = False
-                 is_not_term _ = True 
+                 generated (InputFile _) = False
+                 generated _ = True 
 
-flatten :: [[a]] -> [a]
-flatten l = foldl (++) [] l
 
-execution::Tree->IO [AnchoredRule]
-execution Term = return []
-execution (Node r childs) = do
-		c <- sequence $ map execution childs
-		return ( (flatten c) ++ [r])
 
-make::[Rule]->File->IO [AnchoredRule]
-make rules file =  do
-	s <- (select $ rules_to_tree rules file)
-	execution s
-	
 
---	let tree = rules_to_tree rules file in
---				   let selected = select tree in
---				   execution selected
+execution::DepGraph->IO [([File], Cmd, File) ]
+execution (InputFile _) = return []
+execution (GeneratedFile deps o cmd)  = do
+        c <- sequence $ map execution deps
+        return ( (flatten c) ++ [ (inputs, cmd, o )])
+        where
+            flatten :: [[a]] -> [a]
+            flatten l = foldl (++) [] l
+            inputs = map output deps
 
-instance Show Rule where
-    show (Rule output cmdGen) = 
-    	let (inputs, cmd) = cmdGen ( anchored_parameters output ) in
-    	show output ++  "->" ++ " " ++ (join " " ( map show inputs )) 
 
+napi_log y m d = InputFile $ "napi_log-" ++ show y ++ "-" ++ show m ++ "-" ++ show d
+
+user y m d = GeneratedFile deps myOutput cmd
+  where
+    cmd = "grep 'user'"
+    deps = [napi_log y m d]
+    myOutput = "user-" ++ show y ++ "-" ++ show m ++ "-" ++ show d
+
+
+monthly_user y m = GeneratedFile deps myOutput cmd
+  where
+    cmd = "sort | uniq "
+    deps = [user y m d | d <- day_of_month y m]
+    myOutput = "monthly-user-" ++ show y ++ "-" ++ show m
+
+day_of_month y m = [1..2]
+
+
+
+
+main = do 
+    g <- select $ monthly_user 2012 5
+    g2 <- execution g
+    print g2
