3bb99a17f8a371aac13218fba09cde5bfde86c7b
Peter Halacsy <peter.halacsy@prezi.com>
2012-08-15 12:15:40 +0200

    Towards user database

--- main.hs
+++ main.hs
@@ -9,6 +9,7 @@ import Prelude hiding (elem)
 kpiCodesWithUserActivity::[Int]
 kpiCodesWithUserActivity = [0,1, 2,3,4,5,6,10,11,13, 14, 17,19, 20, 21, 30]
 
+type DaylyFile = Int->Int->Int->DepGraph
 
 kpi_log y m d = InputFile $ printf "/scribe/kpi/kpi-%04d-%02d-%02d_00000" y m d
 
@@ -18,25 +19,34 @@ kpi_code = 4
 kpi_user = 5
 kpi_prezi = 6
 
-daily_uniq_users ::Int->Int->Int->DepGraph
+--base = "/user/hp/"
+base = "/Users/hp/"
+out_base name = base ++ name ++ "-%04d-%02d-%02d"
+
+daily_uniq_users ::DaylyFile
 daily_uniq_users  y m d = pig ( elem 4 kpiCodesWithUserActivity ->> distinct [5] )  
                               [kpi_log y m d] 
-                              (printf "/user/hp/daily_uniq_users-%04d-%02d-%02d" y m d )
+                              (printf (out_base "daily_uniq_users") y m d )
 
 monthly_uniq_users::Int->Int->DepGraph
 monthly_uniq_users y m = pig (distinct [0])
                              [daily_uniq_users y m d | d <- days_of_month y m] 
-                             (printf "/user/hp/monthly_uniq_users-%04d-%02d" y m  )
+                             (printf (out_base "monthly_uniq_users") y m  )
 
 
-daily_prezi_edits::Int->Int->Int->DepGraph
-daily_prezi_edits y m d = pig (eq kpi_code (3::Int) ->> freq [kpi_user, kpi_prezi]) 
+daily_user_prezi_edit_freqs::DaylyFile
+daily_user_prezi_edit_freqs y m d = pig (eq kpi_code (3::Int) ->> freq [kpi_user, kpi_prezi]) 
                               [vacak y m d] 
-                              (printf "/user/hp/daily_prezi_edits-%04d-%02d-%02d" y m d )
+                              (printf (out_base "daily_user_prezi_edit_freqs") y m d )
 
+
+daily_user_save_counts::DaylyFile
+daily_user_save_counts y m d = pig (sum_by 2 [0]) 
+                               [daily_user_prezi_edit_freqs y m d]
+                               (printf (out_base "daily_user_save_counts" ) y m d )
 
 main = do
-  doIt $ daily_prezi_edits 2012 05 28
+  doIt $ daily_user_save_counts 2012 05 27
 
 
     
