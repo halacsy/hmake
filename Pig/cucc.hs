2317373d75ac712a3b4fd6fed330e35b455fdafb
Peter Halacsy <peter.halacsy@prezi.com>
2012-08-17 15:00:22 +0200

    type safe second version

---
+++ Pig/cucc.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+import Data.Maybe
+
+data Exp = IA Int | SA String | Sub Exp Exp | Selector Selector | Sum Exp | Count Exp deriving (Show, Eq)
+
+
+data Selector = Pos Int | Name String deriving (Show, Eq)
+
+
+data Typ = I | S | T RTyp | B RTyp deriving (Show, Eq)
+type Name = Maybe String
+type NamedT = (Name, Typ)
+type RTyp = [NamedT]
+
+
+
+data PipeCmd = Generate [Exp] Pipe | GroupBy [Exp] Pipe | Load String deriving (Show)
+
+type Pipe = (RTyp,PipeCmd)
+
+instance Monad (Either String) where
+  return v = Right v
+  fail s = Left s
+  (Left s) >>= _ = Left s
+  (Right v) >>= f = f v
+
+
+type PRTyp = Either String RTyp
+myConcat::PRTyp ->PRTyp ->PRTyp 
+myConcat (Left s) _ = Left s
+myConcat _ (Left s ) = Left s
+myConcat (Right t1) (Right t2) = (Right (t1 ++t2)) 
+
+typeOf::Exp->Pipe->Either String NamedT
+typeOf (IA _) _ = Right (Nothing, I)
+typeOf (SA _) _ = Right (Nothing, S)
+typeOf (Sub exp1 exp2) p = do
+     (_, t1) <- typeOf exp1 p
+     (_, t2) <- typeOf exp2 p
+     if t1 == t2 then return (Nothing, t1) else fail $ "uncombatible types" ++ (show  exp1) ++ " " ++  (show exp2) 
+
+typeOf (Selector (Pos x) ) (t, _) =  if x > length t - 1 
+                                    then fail $ "out of index " ++ (show x) 
+                                   else
+                                    return (t !! x)
+
+typeOf (Selector (Name n)) (t, _) = case lookup (Just n) t of  
+                                        Just v -> return (Just n, v)
+                                        Nothing -> fail $ "don't find: " ++ n
+
+generate::[Exp]->Pipe->Either String Pipe
+generate xs p = case gen_type of 
+        Left s -> Left s
+        Right t ->  Right (t, Generate xs p)
+        where
+            gen_type::Either String [NamedT]
+            gen_type =  sequence $ map (\t -> typeOf t p) xs
+
+groupBy::[Exp]->Pipe->Either String Pipe
+groupBy xs p = case gen_type of
+        Left s -> Left s
+        Right t -> Right (t, GroupBy xs p)
+        where
+            gen_type::Either String [NamedT]
+            gen_type =  myConcat groupType  groupValueType
+ 
+            groupType::Either String [NamedT]
+            groupType = if length xs == 1 then 
+                          do
+                            (_, x) <- typeOf (head xs) p
+                            return [(Just "group", x)]
+                        else 
+                            -- if there are more elements pig creates a tuple
+                            do
+                                x <- sequence $ map (\t -> typeOf t p) xs
+                                return [(Just "group", T x )]
+            groupValueType::Either String [NamedT]
+            groupValueType = let (orig_typ, _) = p in 
+                             Right [(Just "elements", B orig_typ)]
+
+
+load::String->RTyp->Either String Pipe
+load s t = Right (t, Load s)
+
+a::Either String Pipe
+a =  load "vacak" [(Just "user_id", I), (Just "prezi_id", S), (Just "freq", I)] >>= generate [Selector (Pos 1), Selector (Name "freq")] >>= groupBy [Selector (Pos 1)] 
+
+main = do
+    case a of
+        Left s -> print s
+        Right p -> print p
